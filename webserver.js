a36f5a78cde9ce376623f4bd863fee85cb67247f
taramonheim <taramonheim@me.com>
2020-04-23 18:18:16 +0200

    Front End Full Stack

---
+++ webserver.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(8080);
